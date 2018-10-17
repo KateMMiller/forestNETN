4cd94de236791e660e8f1261b505b9121774306f
KateMMiller <kathryn_miller@nps.gov>
2018-10-17 11:04:28 -0400

    Added summary function, simplified joinQuadData, added invasive filter, and added progress bar to importData function.

--- R/joinQuadData.R
+++ R/joinQuadData.R
@@ -1,16 +1,17 @@
 #' @include joinLocEvent.R
 #' @title joinQuadData
 #'
-#' @description This function combines quadrat species data with species names and calculates average cover and quadrat frequency for each species. Average cover is corrected for number of quadrats sampled.
+#' @description This function combines quadrat species data with species names and allows you to filter on species types, park, years, and visit type. Note that the Shrub guild also includes woody vine species.
 #'
 #' @param speciesType Allows you to filter on native, exotic or include all species.
 #' \describe{
 #' \item{"all"}{Default. Returns all species.}
 #' \item{"native"}{Returns native species only}
 #' \item{"exotic"}{Returns exotic species only}
+#' \item{"invasive"}{Returns species on the Indicator Invasive List}
 #' }
 #'
-#' @return Returns a dataframe with average quadrat cover and frequency for each species and guild for each species.
+#' @return Returns a dataframe with cover class midpoints for each quadrat and includes guild for each species.
 #'
 #' @export
 #'
@@ -19,17 +20,18 @@
 #------------------------
 joinQuadData<-function(speciesType='all', park='all',from=2006, to=2018, QAQC=FALSE, locType='VS', output){
   # Prepare the quadrat data
-  # calculate correct number of quadrats to divide by for average.
   quadsamp$numHerbPlots<-apply(quadsamp[,c(15:22)], 1,sum)
   park.plots<-force(joinLocEvent(park=park, from=from,to=to,QAQC=QAQC,locType=locType, output='short'))
+
   quads1<-merge(park.plots, quadsamp[,c("Event_ID","numHerbPlots")], by="Event_ID", all.x=T)
+
   plants<-plants %>% mutate(Shrub=ifelse(Shrub==1|Vine==1,1,0),
     Tree=ifelse(Latin_Name=="Rhamnus cathartica",0,Tree))
 
-  #------Quadrat Cover
   quadspp<-merge(quads[,c("Event_ID","TSN","qUC_Cover_Class_ID","qUL_Cover_Class_ID","qML_Cover_Class_ID",
     "qBL_Cover_Class_ID","qBC_Cover_Class_ID","qBR_Cover_Class_ID","qMR_Cover_Class_ID","qUR_Cover_Class_ID")],
-    plants[,c("TSN","Latin_Name","Tree","Shrub","Vine","Herbaceous","Graminoid","Exotic","Indicator_Invasive_NETN")], by="TSN",all.x=T)
+    plants[,c("TSN","Latin_Name","Tree","Shrub","Vine","Herbaceous","Graminoid","Exotic","Indicator_Invasive_NETN")],
+    by="TSN",all.x=T)
   quads2<-merge(quads1,quadspp,by="Event_ID",all.x=T)
 
   # Convert coverclasses to midpoints for all 8 quadrats
@@ -43,29 +45,20 @@ joinQuadData<-function(speciesType='all', park='all',from=2006, to=2018, QAQC=FA
   quads2[,14:21][quads2[,14:21]==8]<-85
   quads2[,14:21][quads2[,14:21]==9]<-97.5
 
-  # average cover by species, so I can later summarize by guild
-  quads3<-quads2 %>% group_by(Event_ID,TSN,Latin_Name) %>%
-    mutate(avecov=(qUC_Cover_Class_ID+qUR_Cover_Class_ID+qMR_Cover_Class_ID+qBR_Cover_Class_ID+
-        qBC_Cover_Class_ID+qBL_Cover_Class_ID+qML_Cover_Class_ID+qUL_Cover_Class_ID)/numHerbPlots) %>%
-    dplyr::select(Event_ID,TSN,Latin_Name,Exotic,Tree,Shrub,Herbaceous,Graminoid,Indicator_Invasive_NETN,avecov) %>%
-    mutate(Tree=ifelse(Latin_Name=="Rhamnus cathartica",0,Tree))
+  old.names<-names(quads2[,14:21])
+  new.names<-c('UC','UL','ML','BL','BC','BR','MR','UR')
+  quads2<-quads2 %>% rename_at(vars(old.names),~new.names)
 
-  # calculate frequency
-  quads2[,14:21][!is.na(quads2[,14:21]) & quads2[,14:21]>0]<-1
+  quads3<-if (speciesType=='native'){filter(quads2,Exotic==FALSE)
+  } else if (speciesType=='exotic'){filter(quads2,Exotic==TRUE)
+  } else if (speciesType=='invasive'){filter(quads2,Indicator_Invasive_NETN==TRUE)
+  } else if (speciesType=='all'){(quads2)
+  } else if (speciesType!='native'|speciesType!='exotic'|speciesType!='invasive'|speciesType!='all'){
+    stop("speciesType must be either 'native','exotic', 'invasive', or 'all'")}
 
-  quads4<-quads2 %>% group_by(Event_ID,TSN) %>%
-    mutate(quadfreq=(qUC_Cover_Class_ID+qUR_Cover_Class_ID+qMR_Cover_Class_ID+qBR_Cover_Class_ID+
-        qBC_Cover_Class_ID+qBL_Cover_Class_ID+qML_Cover_Class_ID+qUL_Cover_Class_ID), numHerbPlots=first(numHerbPlots)) %>%
-    dplyr::select(Event_ID,TSN,quadfreq) %>% left_join(quads3,.,by=c("Event_ID","TSN"))
+  quads4<-merge(quads1,quads3[,c(1,13:29)],by='Event_ID',all.x=T)
+  quads4[,c(14:21, 23:29)][is.na(quads4[,c(14:21, 23:29)])]<-0
+  return(data.frame(quads4))
 
-  quads5<-if (speciesType=='native'){filter(quads4,Exotic==FALSE)
-  } else if (speciesType=='exotic'){filter(quads4,Exotic==TRUE)
-  } else if (speciesType=='all'){(quads4)
-  } else if (speciesType!='native'|speciesType!='exotic'|speciesType!='all'){
-    stop("speciesType must be either 'native','exotic', or 'all'")}
-
-  quads6<-merge(quads1,quads5,by='Event_ID',all.x=T)
-  quads6[,21:22][is.na(quads6[,21:22])]<-0
-  return(data.frame(quads6))
 } # end of function
 
---
+++ R/sumQuadGuilds.R
@@ -0,0 +1,82 @@
+#' @include joinQuadData.R
+#' @title sumQuadGuilds
+#'
+#' @description This function summarizes output from joinQuadData and calculates average cover and quadrat frequency for each guild. Average cover is corrected for number of quadrats sampled.
+#'
+#' @param speciesType Allows you to filter on native, exotic or include all species.
+#' \describe{
+#' \item{"all"}{Default. Returns all species.}
+#' \item{"native"}{Returns native species only}
+#' \item{"exotic"}{Returns exotic species only}
+#' \item{"invasive"}{Returns species on the Indicator Invasive List}
+#' }
+#'
+#' @return Returns a dataframe with average quadrat cover, percent quadrat frequency and quadrat frequency count for tree,shrub/vine,herbaceous,and graminoid.
+#'
+#' @export
+#'
+#------------------------
+# Joins quadrat tables and filters by park, year, and plot/visit type
+#------------------------
+sumQuadGuilds<-function(speciesType='all', park='all',from=2006, to=2018, QAQC=FALSE, locType='VS', output,...){
+  # Prepare the quadrat data
+  park.plots<-force(joinLocEvent(park=park,from=from,to=to,QAQC=QAQC,locType=locType,output='short'))
+  quads1<-force(joinQuadData(park=park, from=from,to=to,QAQC=QAQC,locType=locType,speciesType=speciesType,
+    output='short'))
+  quads1<-quads1 %>% mutate(Shrub=ifelse(Shrub+Vine>0,1,0)) %>% select(-Vine)
+
+  # gather to put quadrats all in one column
+  quads2<-quads1 %>% select(Event_ID:Event_QAQC,numHerbPlots,UC:UR,Tree:Indicator_Invasive_NETN) %>%
+    gather(key='quad',value='pctcov',UC:UR)
+
+  # replace 1/0 for guilds with percent cover values where they're present
+  quads3<-quads2 %>% mutate(Tree=ifelse(Tree==1,pctcov,0),Shrub=ifelse(Shrub==1,pctcov,0),
+    Herbaceous=ifelse(Herbaceous==1,pctcov,0), Graminoid=ifelse(Graminoid==1,pctcov,0))
+
+  # gather to put all guild in one column. Have to remove 0s
+  quads4<-quads3 %>% select(Event_ID:Graminoid,quad) %>% gather(key=guild,value=cover,Tree:Graminoid) %>%
+    filter(cover>0)
+
+  # summarise cover by quadrat and guild. eg if two shrubs are present in the same quad, their cover is summed
+  quads5<-quads4 %>% select(Event_ID,Plot_Name,Year,numHerbPlots:cover) %>%
+    group_by(Event_ID,Plot_Name,Year,quad,guild) %>%
+    summarise(cover=sum(cover),numHerbPlots=first(numHerbPlots))
+
+  # calculate average quadrat cover and frequency
+  quads6<-quads5 %>% group_by(Event_ID,Plot_Name,Year,guild) %>%
+    summarize(avecov=sum(cover)/first(numHerbPlots),quadfreq=length(cover>0),
+    avefreq=length(cover>0)/first(numHerbPlots))
+
+  # wrangle data so that the end result is one guild column with tree, shrub, herb, gram and one field for each
+  # percent cover, percent frequency and quadrat frequency count.
+  # percent cover
+  quads.pctcov<-quads6 %>% select(Event_ID:guild,avecov) %>% spread(guild,avecov,fill=0) %>% arrange(Plot_Name,Year)
+  quads.pctcov2<-merge(park.plots,quads.pctcov[,c("Event_ID","Graminoid","Herbaceous","Shrub","Tree")],
+    by=c("Event_ID"), all.x=T)
+  quads.pctcov2[,12:15][is.na(quads.pctcov2[,12:15])]<-0
+  quads.pctcov3<-quads.pctcov2 %>% gather(guild,avecov,Graminoid:Tree)
+  # quadrat frequency
+  quads.freq<-quads6 %>% select(Event_ID:guild,quadfreq) %>% spread(guild,quadfreq,fill=0) %>% arrange(Plot_Name,Year)
+  quads.freq2<-merge(park.plots,quads.freq[,c("Event_ID","Graminoid","Herbaceous","Shrub","Tree")],
+    by=c("Event_ID"), all.x=T)
+  quads.freq2[,12:15][is.na(quads.freq2[,12:15])]<-0
+  quads.freq3<-quads.freq2 %>% gather(guild,quadfreq,Graminoid:Tree)
+  head(quads.freq3)
+  # quadrat percent frequency
+  quads.pctfreq<-quads6 %>% select(Event_ID:guild,avefreq) %>% spread(guild,avefreq,fill=0) %>% arrange(Plot_Name,Year)
+  quads.pctfreq2<-merge(park.plots,quads.pctfreq[,c("Event_ID","Graminoid","Herbaceous","Shrub","Tree")],
+    by=c("Event_ID"), all.x=T)
+  quads.pctfreq2[,12:15][is.na(quads.pctfreq2[,12:15])]<-0
+  quads.pctfreq3<-quads.pctfreq2 %>% gather(guild,avefreq,Graminoid:Tree)
+
+  #combine to get final dataframe
+  quad.comb1<-merge(quads.pctcov3,quads.freq3[,c("Event_ID","guild","quadfreq")],
+    by=c("Event_ID","guild"), all.x=T,all.y=T)
+  quad.comb2<-merge(quad.comb1,quads.pctfreq3[,c("Event_ID","guild","avefreq")],
+    by=c("Event_ID","guild"), all.x=T,all.y=T)
+  quads.final<-quad.comb2 %>% select(Location_ID,Event_ID,Unit_Code:cycle,guild,avecov:avefreq)
+
+  head(quads.final)
+  return(data.frame(quads.final))
+} # end of function
+
