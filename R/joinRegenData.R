11ad6485eac660197bc2b13d5a17ca30840d9836
KateMMiller <kathryn_miller@nps.gov>
2020-12-09 10:00:47 -0500

    stocking index fix

--- R/joinRegenData.R
+++ R/joinRegenData.R
@@ -53,51 +53,63 @@ joinRegenData<-function(speciesType=c('all', 'native','exotic'), canopyForm=c('c
   units<-match.arg(units)
 
 # Prepare the seedling data
-  seeds1<-merge(sdlg,micro, by="Microplot_Characterization_Data_ID",all.x=T)
+  seeds1<-merge(sdlg, micro, by="Microplot_Characterization_Data_ID", all.x=T, all.y = T)
   seeds1$Num_Seedlings_15_30cm[is.na(seeds1$Num_Seedlings_15_30cm)]<-0
   seeds1$Num_Seedlings_30_100cm[is.na(seeds1$Num_Seedlings_30_100cm)]<-0
   seeds1$Num_Seedlings_100_150cm[is.na(seeds1$Num_Seedlings_100_150cm)]<-0
   seeds1$Num_Seedlings_Above_150cm[is.na(seeds1$Num_Seedlings_Above_150cm)]<-0
 
-  seeds2<-seeds1 %>% group_by(Event_ID,TSN,Microplot_Name) %>% summarise(seed15.30=sum(Num_Seedlings_15_30cm),
-    seed30.100=sum(Num_Seedlings_30_100cm),seed100.150=sum(Num_Seedlings_100_150cm),
-    seed150p=sum(Num_Seedlings_Above_150cm))
+  seeds2<-seeds1 %>% group_by(Event_ID, TSN, Microplot_Name) %>%
+    summarise(seed15.30 = sum(Num_Seedlings_15_30cm),
+              seed30.100 = sum(Num_Seedlings_30_100cm),
+              seed100.150 = sum(Num_Seedlings_100_150cm),
+              seed150p = sum(Num_Seedlings_Above_150cm))
 
 # Prepare the sapling data
-  saps1<-merge(micro,saps,by="Microplot_Characterization_Data_ID", all.y=T)
-  saps1<-saps1 %>% mutate(sap=ifelse(Count>0 & !is.na(Count),Count,ifelse(DBH>0 & !is.na(DBH),1,0)))
-  saps2<-saps1 %>% group_by(Event_ID,TSN, Microplot_Name) %>%
+  saps1<-merge(micro,saps,by="Microplot_Characterization_Data_ID", all.x = T, all.y=T)
+  saps1<-saps1 %>% mutate(sap = ifelse(Count>0 & !is.na(Count),
+                                       Count,
+                                       ifelse(DBH>0 & !is.na(DBH),1,0)))
+  saps2<-saps1 %>% group_by(Event_ID, TSN, Microplot_Name) %>%
     summarise(sap.stems=sum(sap, na.rm=T),avg.sap.dbh=mean(DBH, na.rm=T))
 
 # Combine seedling and sapling data
   park.plots<-force(joinLocEvent(park=park, from=from,to=to,QAQC=QAQC,locType=locType,panels=panels,output='short'))
   regen1<-merge(park.plots,seeds2,by='Event_ID', all.x=T,all.y=F)
   regen2<-merge(regen1,saps2,by=c("Event_ID","TSN", "Microplot_Name"),all.x=T,all.y=T)
-  regen3<-merge(regen2,plants[,c('TSN','Latin_Name','Common','Exotic','Canopy_Exclusion')], by='TSN',all.x=T)
+  regen3a<-merge(regen2[,c("Event_ID", "TSN", "Microplot_Name", "seed15.30", "seed30.100", "seed100.150", "seed150p",
+                           "sap.stems", "avg.sap.dbh")],
+                 plants[,c('TSN','Latin_Name','Common','Exotic','Canopy_Exclusion')], by='TSN',all.x=T)
+
+  regen3 <- merge(park.plots, regen3a, by = intersect(names(park.plots), names(regen3a)), all.x = T, all.y = F)
+  regen3[,14:19][is.na(regen3[,14:19])]<-0
 
   regen4<- if (numMicros==1) {filter(regen3, Microplot_Name=='UR') %>% droplevels() # randomly determined this
   } else if (numMicros==2) {filter(regen3, Microplot_Name %in% c('UR','B')) %>% droplevels() #randomly determined this
   } else if (numMicros==3) {regen3}
 
-
-  regen5<-if(canopyForm=='canopy'){filter(regen4, Canopy_Exclusion!=1)
+  regen5<-if(canopyForm=='canopy'){filter(regen4, Canopy_Exclusion==FALSE)
   } else if(canopyForm=='all'){(regen4)
   }
 
-  regen6<- if (speciesType=='native'){filter(regen5,Exotic==FALSE)
-  } else if (speciesType=='exotic'){filter(regen5,Exotic==TRUE)
+  regen6<- if (speciesType=='native'){filter(regen5, Exotic==FALSE)
+  } else if (speciesType=='exotic'){filter(regen5, Exotic==TRUE)
   } else if (speciesType=='all'){(regen5)
   }
 
-  regen6[,14:18][is.na(regen6[,14:18])]<-0
-  regen6<-regen6 %>% mutate(Latin_Name= ifelse((Plot_Name=="SAGA-008" & Year==2010),
-                                               paste0('MissingData'), paste0(Latin_Name)),
-                            Common= ifelse(is.na(Common), paste0(Latin_Name), paste0(Common)))
+  regen6[,14:19][is.na(regen6[,14:19])]<-0
+  regen6$Latin_Name[regen6$Plot_Name == "SAGA-008" & regen6$Year == 2010] <- "MissingData"
+
+  regen6 <- regen6 %>% mutate(Common = ifelse(is.na(Common), paste0(Latin_Name), paste0(Common)))
+
   # Summarise data at plot level. We lose the Microplot name, but average over # microplots selected in next step
   regen7<-regen6 %>% group_by(Event_ID, TSN, Latin_Name, Common, Exotic, Canopy_Exclusion, Year, cycle) %>%
     summarise(seed15.30=sum(seed15.30, na.rm=T),seed30.100=sum(seed30.100, na.rm=T),seed100.150=sum(seed100.150, na.rm=T),
               seed150p=sum(seed150p, na.rm=T), sap.stems=sum(sap.stems, na.rm=T), avg.sap.dbh=mean(avg.sap.dbh, na.rm=T)) %>%
     ungroup()
+
+
+  regen7[,9:14][is.na(regen7[,9:14])]<-0
 
   regen8<-regen7 %>% mutate(micro=ifelse(Year==2006, 1, numMicros),
            stock=((1*seed15.30)+(2*seed30.100)+(20*seed100.150)+(50*seed150p)+(50*sap.stems))/micro,
