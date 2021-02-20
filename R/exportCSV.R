e813846d96f3037000bc7715154ca9db38600d34
KateMMiller <kathryn_miller@nps.gov>
2021-02-20 14:04:35 -0500

    added zip export and more error handling

--- R/exportCSV.R
+++ R/exportCSV.R
@@ -1,8 +1,8 @@
 #' @title exportCSV: Export SQL views to .csv
 #'
-#' @description This function either exports database views to individual .csv files, or exports them as a zip file
-#' with the database name (NETN_Forest), and the date it was zipped. The exported .csv files can then be imported
-#' via importCSV. This function is primarily for internal use to create flat files that users can import to run
+#' @description This function exports NETN_Forest database views either to individual .csv files, or as a zip file
+#' with the database name (NETN_Forest), and the date it was zipped. The exported .csv or .zip files can then be imported
+#' via importCSV(). This function is primarily for internal use to create flat files that users can import to run
 #' functions in this package without having a connection to the database server. This is particularly useful for external
 #' users who don't have access to NPS servers.
 #'
@@ -10,9 +10,11 @@
 #'
 #' @param zip Logical. If TRUE, exports a zip file. If FALSE (Default), exports individual csvs.
 #'
+#' @return NETN database views exported to specified path
+#'
 #' @examples
 #' # RUN FIRST
-#' library(forestMIDN)
+#' library(forestNETN)
 #' importData()
 #'
 #' # Export csvs to working directory
@@ -21,8 +23,8 @@
 #' # Export a zip to the path specified
 #' exportCSV(path = "C:/Forest_Health/exports/NETN", zip = TRUE)
 #'
-#' # Export views as .csvs to working directory
-#' exportCSV()
+#' # Export views as .csvs to specified path
+#' exportCSV(path = "C:/Forest_Health/exports/NETN")
 #'
 #' @export
 
@@ -33,29 +35,32 @@ exportCSV<- function(path = NA, zip = FALSE){
     stop("Package 'zip' needed to export to zip file. Please install it.", call. = FALSE)
   }
 
+  # Make sure all the views are loaded. If anything is missing, function stops.
+  view_list <- c("NETN_StandInfoPhotos", "NETN_MicroplotSeedlings", "COMN_TreesByEvent", "COMN_QuadCharacter",
+                 "COMN_TreesFoliageCondFlat", "COMN_StandPlantCoverStrata", "COMN_StandDisturbances", "COMN_CWD",
+                 "COMN_StandTreeHeights", "COMN_StandForestFloor", "COMN_MicroplotShrubs", "COMN_TreesConditionsFlat",
+                 "COMN_QuadNotes", "NETN_MicroplotSaplings", "COMN_StandSlopes", "COMN_AddtionalSpecies",
+                 "NETN_MicroplotSaplingsCount", "dsTreeByCondition", "NETN_QuadSpecies", "COMN_Plots",
+                 "COMN_Events", "COMN_TreesVine")
+
+  files <- if(exists("VIEWS_NETN")){ls(envir = VIEWS_NETN)} else {ls()}
+
+  missing <- setdiff(view_list, files)
+
+  if(length(missing) > 0 & length(missing) < 22){
+    stop(paste0("Missing the following views: ", paste0(missing, collapse = ", ")))
+  } else if (length(missing) >= 22){
+    stop("Views were not detected in your workspace. Please import the data first.")}
+
   # Error handling for path
   if(is.na(path)){path <- getwd()
-    print(paste0("No path specified. Output saved to working directory: ", getwd()), quote = FALSE)
+  print(paste0("No path specified. Output saved to working directory: ", getwd()), quote = FALSE)
   } else if(!dir.exists(path)){
-      stop("Specified directory does not exist.")}
-    else{print(paste0("Output saved to ", path), quote = FALSE)}
+      stop("Specified directory does not exist.")
+  } else{print(paste0("Output saving to ", path), quote = FALSE)}
 
   # Add / to end of path if it wasn't specified.
   path <- if(substr(path,nchar(path),nchar(path))!="/"){paste0(path,"/")} else {(paste0(path))}
-
-  # File export. Look for VIEWS_NETN env first. If doesn't exist, look for tables in glob.env.
-  if(exists("VIEWS_NETN")){
-    view_list <- names(VIEWS_NETN)
-  } else if(exists("COMN_Plots")){ # check if a core view has been loaded in global environment.
-                                   # hardcoding the view_list here, because can't assume only these objects are in glob.env.
-                                   # also avoiding having to open another connection to database
-    view_list <- c("NETN_StandInfoPhotos", "NETN_MicroplotSeedlings", "COMN_TreesByEvent", "COMN_QuadCharacter",
-                   "COMN_TreesFoliageCondFlat", "COMN_StandPlantCoverStrata", "COMN_StandDisturbances", "COMN_CWD",
-                   "COMN_StandTreeHeights", "COMN_StandForestFloor", "COMN_MicroplotShrubs", "COMN_TreesConditionsFlat",
-                   "COMN_QuadNotes", "NETN_MicroplotSaplings", "COMN_StandSlopes", "COMN_AddtionalSpecies",
-                   "NETN_MicroplotSaplingsCount", "dsTreeByCondition", "NETN_QuadSpecies", "COMN_Plots",
-                   "COMN_Events", "COMN_TreesVine")
-    } else {stop("Views were not detected in your workspace. Please run importData() first.")}
 
   # Set up progress bar
   pb <- txtProgressBar(min = 0, max = length(view_list), style = 3)
@@ -67,7 +72,9 @@ exportCSV<- function(path = NA, zip = FALSE){
   if(zip == FALSE){
   invisible(lapply(seq_along(view_list), function(x){
     setTxtProgressBar(pb, x)
-    write.csv(mget(view_list[[x]], envir = env), paste0(path, view_list[x], ".csv"))
+    write.csv(get(view_list[[x]], envir = env),
+              paste0(path, view_list[x], ".csv"),
+              row.names = FALSE)
   }))
  } else if(zip == TRUE){ #create tmp dir to export csvs, bundle to zip, then delete tmp folder
 
@@ -75,7 +82,9 @@ exportCSV<- function(path = NA, zip = FALSE){
 
    invisible(lapply(seq_along(view_list), function(x){
     setTxtProgressBar(pb, x)
-    write.csv(mget(view_list[[x]], envir = env),paste0(tmp, "\\", view_list[x], ".csv"))}))
+    write.csv(get(view_list[[x]], envir = env),
+              paste0(tmp, "\\", view_list[x], ".csv"),
+              row.names = FALSE)}))
 
   file_list <- list.files(tmp)
 
